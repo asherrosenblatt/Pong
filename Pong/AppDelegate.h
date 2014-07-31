39c71de6e239813109319569249851557759e45a
asherrosenblatt <asher.rosenblatt@me.com>
2014-07-31 10:54:58 -0500

    Initial Commit

---
+++ Pong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pong
+//
+//  Created by Asher Rosenblatt on 7/31/14.
+//  Copyright (c) 2014 Asher Rosenblatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pong/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pong
+//
+//  Created by Asher Rosenblatt on 7/31/14.
+//  Copyright (c) 2014 Asher Rosenblatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
